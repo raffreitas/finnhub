def8f153d1026b3d46c39907c4be84ec18c54d4f
Rafael Freitas <raffreitasx@gmail.com>
2025-06-07 09:20:09 -0300

    Add unit tests for Portfolio transaction creation
    Introduce `PortfolioTests` class with a test method
    to validate the creation of buy transactions.
    Add `PortifolioTestsFixture` for generating test data
    using `Faker`. Update project file to reference the
    main domain project for access to necessary classes.

---
+++ src/contexts/portfolio-management/test/FinnHub.PortfolioManagement.Domain.UnitTests/Aggregates/PortfolioTests.cs
@@ -0,0 +1,34 @@
+﻿using FinnHub.PortfolioManagement.Domain.Aggregates.Entities;
+using FinnHub.PortfolioManagement.Domain.Aggregates.Enums;
+using FinnHub.PortfolioManagement.Domain.Aggregates.ValueObjects;
+
+using Shouldly;
+
+namespace FinnHub.PortfolioManagement.Domain.UnitTests.Aggregates;
+
+public class PortfolioTests(PortifolioTestsFixture fixture) : IClassFixture<PortifolioTestsFixture>
+{
+    [Fact(DisplayName = nameof(CreateBuyTransaction_ShouldCreateTransaction_WhenValidParameters))]
+    [Trait("Domain", "Portfolio - Aggregates")]
+    public void CreateBuyTransaction_ShouldCreateTransaction_WhenValidParameters()
+    {
+        // Arrange
+        var portfolioId = fixture.Faker.Random.Guid();
+        var assetSymbol = AssetSymbol.Create(fixture.Faker.Commerce.ProductName()[..10]);
+        var quantity = Quantity.Create(fixture.Faker.Random.Int(1, 100));
+        var price = Money.Create(fixture.Faker.Random.Decimal(1.0m, 1000.0m));
+        var transactionDate = DateTimeOffset.UtcNow;
+        // Act
+
+        var transaction = Transaction.CreateBuyTransaction(portfolioId, assetSymbol, quantity, price, transactionDate);
+
+        // Assert
+        portfolioId.ShouldBe(transaction.PortfolioId);
+        assetSymbol.ShouldBe(transaction.AssetSymbol);
+        transaction.Type.ShouldBe(TransactionType.Buy);
+        quantity.ShouldBe(transaction.Quantity);
+        price.ShouldBe(transaction.Price);
+        transaction.TransactionDate.ShouldBe(transactionDate);
+        transaction.IsSettled.ShouldBeFalse();
+    }
+}
---
+++ src/contexts/portfolio-management/test/FinnHub.PortfolioManagement.Domain.UnitTests/Aggregates/PortifolioTestsFixture.cs
@@ -0,0 +1,7 @@
+﻿using Bogus;
+
+namespace FinnHub.PortfolioManagement.Domain.UnitTests.Aggregates;
+public class PortifolioTestsFixture
+{
+    public Faker Faker = new();
+}
