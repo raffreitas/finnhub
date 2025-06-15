e53674ae0148b654b7c962e2f514ee51d8ddee8a
Rafael Freitas <raffreitasx@gmail.com>
2025-06-15 10:07:42 -0300

    feat(seedwork): add pagination records for API responses
    - Introduced `PaginatedParams` record with `PageNumber` and `PageSize` properties, defaulting `PageSize` to 10.
    - Added `PaginatedResult<T>` generic record to encapsulate pagination details, including properties for current page, page size, total count, total pages, and navigation flags.
    - Implemented a constructor in `PaginatedResult<T>` to initialize pagination properties and an `Items` property to hold the current page's items.

---
+++ src/contexts/portfolio-management/src/FinnHub.PortfolioManagement.Domain/SeedWork/PaginatedParams.cs
@@ -0,0 +1,7 @@
+﻿namespace FinnHub.PortfolioManagement.Domain.SeedWork;
+
+public sealed record PaginatedParams
+{
+    public int PageNumber { get; init; }
+    public int PageSize { get; init; } = 10;
+}
---
+++ src/contexts/portfolio-management/src/FinnHub.PortfolioManagement.Domain/SeedWork/PaginatedResult.cs
@@ -0,0 +1,19 @@
+﻿namespace FinnHub.PortfolioManagement.Domain.SeedWork;
+public record PaginatedResult<T>
+{
+    public int Page { get; init; }
+    public int PageSize { get; init; }
+    public int TotalCount { get; init; }
+    public int TotalPages => (int)Math.Ceiling((double)TotalCount / PageSize);
+    public bool HasPreviousPage => Page > 1;
+    public bool HasNextPage => Page < TotalPages;
+    public IEnumerable<T> Items { get; init; } = [];
+
+    public PaginatedResult(int pageNumber, int pageSize, int totalCount, IEnumerable<T> items)
+    {
+        Page = pageNumber;
+        PageSize = pageSize;
+        TotalCount = totalCount;
+        Items = items;
+    }
+}
