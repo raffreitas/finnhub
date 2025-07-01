9c71bf5d8ba68ab17a355545cd06547a8cc01a51
Rafael Freitas <raffreitasx@gmail.com>
2025-07-01 18:55:26 -0300

    feat(json): add JsonExtensions for serialization methods
    - Added `JsonExtensions.cs` with extension methods for JSON serialization and deserialization using Newtonsoft.Json.
    - Updated package version from `1.0.1` to `1.0.2` in `FinnHub.Shared.Infrastructure.csproj`.
    - Added a new package reference for `Newtonsoft.Json` version `13.0.3`.

---
+++ src/building-blocks/FinnHub.Shared.Infrastructure/Extensions/JsonExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace FinnHub.Shared.Infrastructure.Extensions;
+
+public static class JsonExtensions
+{
+    public static string ToNameTypeJson(this object @object)
+        => JsonConvert.SerializeObject(@object, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All });
+
+    public static string ToJson(this object @object)
+        => JsonConvert.SerializeObject(@object, new JsonSerializerSettings { });
+
+    public static T ToNameTypeObject<T>(this string json)
+        => JsonConvert.DeserializeObject<T>(json, new JsonSerializerSettings
+        {
+            TypeNameHandling = TypeNameHandling.All,
+            MetadataPropertyHandling = MetadataPropertyHandling.ReadAhead
+        })!;
+
+    public static T ToObject<T>(this string json)
+        => JsonConvert.DeserializeObject<T>(json, new JsonSerializerSettings { })!;
+
+    public static async Task<T> ToObjectAsync<T>(this Stream stream, CancellationToken cancellationToken)
+    {
+        using var json = new StreamReader(stream);
+        return JsonConvert.DeserializeObject<T>(await json.ReadToEndAsync(cancellationToken).ConfigureAwait(false))!;
+    }
+
+    public static bool In<T>(this T item, params T[] items)
+        => items.Contains(item);
+}
